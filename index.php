eda646bece6a5c32d8231a78f101623535b5d51e
ruikun <ruikun@staff.sina.com.cn>
2015-09-26 11:09:48 +0800

    add new codes

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+header("Content-type: text/html; charset=utf-8"); 
+$channel = new SaeChannel();
+$connection = $channel->createChannel('test',3600);
+include 'test.html';
+$message_content = 'qwgxiaoxiao';
+$ret = $channel->sendMessage('test',$message_content);
